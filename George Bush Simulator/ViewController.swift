1906131f4f98c906ee26fe7822e773dc99d8c7a4
Camper <Camper@ECCMAC-PA04.local>
2015-07-15 09:30:42 -0400

    eggs

--- George Bush Simulator/ViewController.swift
+++ George Bush Simulator/ViewController.swift
@@ -27,6 +27,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func spooks(sender: AnyObject) {
+        spooks.play()
+    }
     
 }
 
